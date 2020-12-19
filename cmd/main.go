1ca5defe40d2d3b033ecc394fea368341531af99
Mike Mason <michael@michaeljmason.com>
2020-12-18 16:31:59 -0800

    remote package

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/mmason33/go-html-link-parser/link"
+
+	"github.com/mmason33/linkParser"
 )
 
 // https://godoc.org/golang.org/x/net/html#example-Parse
@@ -12,6 +13,6 @@ import (
 func main() {
 	fileFlag := flag.String("file", "./ex1.html", "Path to the html file")
 	flag.Parse()
-	links, err := link.ParseHTMLLinks(*fileFlag)
+	links, err := linkParser.ParseHTMLLinks(*fileFlag)
 	fmt.Println(links, err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/mmason33/goHtmlLinkParser/link
+module github.com/mmason33/linkParser
 
 go 1.15
+
+require golang.org/x/net v0.0.0-20201216054612-986b41b23924
